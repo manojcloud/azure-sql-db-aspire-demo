265a823dc5169c924ae051e1bf3cb93bac9b59e5
Davide Mauri <damauri@microsoft.com>
2025-03-11 11:57:01 -0700

    swtiched to use minimal API for consistency with other samples

--- base/WebApplication1/Program.cs
+++ base/WebApplication1/Program.cs
@@ -1,30 +1,35 @@
-namespace WebApplication1
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.AddServiceDefaults();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
 
-            // Add services to the container.
-            builder.AddServiceDefaults();
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+});
 
-            builder.Services.AddProblemDetails();
+app.Run();
 
-            builder.Services.AddControllers();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
-        }
-    }
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }

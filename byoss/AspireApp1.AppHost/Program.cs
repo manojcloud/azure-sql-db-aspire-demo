c0dbcfef987d4576d64a46ca4727e50fd8e11191
Davide Mauri <damauri@microsoft.com>
2025-04-15 13:57:03 -0700

    added custom urls

--- byoss/AspireApp1.AppHost/Program.cs
+++ byoss/AspireApp1.AppHost/Program.cs
@@ -3,7 +3,17 @@ var builder = DistributedApplication.CreateBuilder(args);
 var db = builder.AddConnectionString("db");
 
 builder.AddProject<Projects.WebApplication1>("webapi")
+    .WithUrlForEndpoint("http", url =>
+    {
+        url.DisplayText = "Forecast API (HTTP)";
+        url.Url += "/weatherforecast";
+    })
+    .WithUrlForEndpoint("https", url =>
+    {
+        url.DisplayText = "Forecast API (HTTPS)";
+        url.Url += "/weatherforecast";
+    })
     .WithReference(db);
-    
+;
 
 builder.Build().Run();

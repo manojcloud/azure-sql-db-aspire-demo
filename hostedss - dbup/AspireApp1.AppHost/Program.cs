6b25f5553f5e6b07671f52fdb12d22949e9cf8f8
Davide Mauri <damauri@microsoft.com>
2025-04-15 14:11:32 -0700

    updated to Aspire 9.2

--- hostedss - dbup/AspireApp1.AppHost/Program.cs
+++ hostedss - dbup/AspireApp1.AppHost/Program.cs
@@ -14,6 +14,16 @@ var dbDeploy = builder.AddProject<Projects.DatabaseDeploy>("dbDeploy")
     .WaitFor(db);
 
 builder.AddProject<Projects.WebApplication1>("webapi")
+    .WithUrlForEndpoint("http", url =>
+    {
+        url.DisplayText = "Forecast API (HTTP)";
+        url.Url += "/weatherforecast";
+    })
+    .WithUrlForEndpoint("https", url =>
+    {
+        url.DisplayText = "Forecast API (HTTPS)";
+        url.Url += "/weatherforecast";
+    })
     .WithReference(db)
     .WaitFor(db)
     .WaitForCompletion(dbDeploy);

cb1a3e1bb95be3348935f73300abb153d7d81ba4
Davide Mauri <damauri@microsoft.com>
2025-04-15 14:07:36 -0700

    updated to Aspire 9.2

--- hostedss/AspireApp1.AppHost/Program.cs
+++ hostedss/AspireApp1.AppHost/Program.cs
@@ -2,13 +2,23 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var sqlPassword = builder.AddParameter("sqlsrv-password");
 
-var sqlSrv = builder.AddSqlServer("sqlsrv", sqlPassword, port:1435).
-    WithLifetime(ContainerLifetime.Persistent);
+var sqlSrv = builder.AddSqlServer("sqlsrv", sqlPassword, port:1435)
+    .WithLifetime(ContainerLifetime.Persistent);
 
 //var db = sqlSrv.AddDatabase("db");
 var db = sqlSrv.AddDatabase("db", databaseName: "tempdb");
 
 builder.AddProject<Projects.WebApplication1>("webapi")
+    .WithUrlForEndpoint("http", url =>
+    {
+        url.DisplayText = "Forecast API (HTTP)";
+        url.Url += "/weatherforecast";
+    })
+    .WithUrlForEndpoint("https", url =>
+    {
+        url.DisplayText = "Forecast API (HTTPS)";
+        url.Url += "/weatherforecast";
+    })
     .WithReference(db)
     .WaitFor(db);
 
